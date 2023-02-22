f5124d9c1c2fde5b44fd64b66ef1b2c01c78df9c
Utkirbek <utkirbel.me@gmail.com>
2023-02-22 14:20:34 +0500

    some changes

--- api/index.js
+++ api/index.js
@@ -49,6 +49,4 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
-// app.listen(PORT, () => console.log(`server running on port ${PORT}`));
-
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
