3cc8547bfd7e3a352f16ffbeafcd88b01276faca
Utkirbek <dovutkhanmutaliyev@gmail.com>
2023-02-28 12:28:43 +0500

    some chnages

--- models/Tour.js
+++ models/Tour.js
@@ -3,94 +3,104 @@ const mongoose = require('mongoose');
 const tourSchema = new mongoose.Schema(
   {
     title: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     going: [
-        {
-            airline: {
-                type: String,
-                required: true,
-            },
-            flightName: {
-                type: String,
-                required: true,
-            },
-            flightWay: {
-                type: String,
-                required: true,
-            },
-            flightDate: {
-                type: String,
-                required: true,
-            },
-            flightTime: {
-                type: String,
-                required: true,
-            },
-            reachTime: {
-                type: String,
-                required: true,
-            },
-        }
-
+      {
+        airline: {
+          type: String,
+          required: true,
+        },
+        flightName: {
+          type: String,
+          required: true,
+        },
+        flightWay: {
+          type: String,
+          required: true,
+        },
+        flightDate: {
+          type: String,
+          required: true,
+        },
+        flightTime: {
+          type: String,
+          required: true,
+        },
+        reachTime: {
+          type: String,
+          required: true,
+        },
+      },
     ],
     coming: [
-        {
-            airline: {
-                type: String,
-                required: true,
-            },
-            flightName: {
-                type: String,
-                required: true,
-            },
-            flightWay: {
-                type: String,
-                required: true,
-            },
-            flightDate: {
-                type: String,
-                required: true,
-            },
-            flightTime: {
-                type: String,
-                required: true,
-            },
-            reachTime: {
-                type: String,
-                required: true,
-            },
-        }
-
+      {
+        airline: {
+          type: String,
+          required: true,
+        },
+        flightName: {
+          type: String,
+          required: true,
+        },
+        flightWay: {
+          type: String,
+          required: true,
+        },
+        flightDate: {
+          type: String,
+          required: true,
+        },
+        flightTime: {
+          type: String,
+          required: true,
+        },
+        reachTime: {
+          type: String,
+          required: true,
+        },
+      },
     ],
     tickets: {
-        amount: {
-            type: Number,
-            required: true,
-        },
-        price: {
-            type: Number,
-            required: true,
-        },
-        totalPrice: {
-            type: Number,
-            required: true,
-        },
-        paid: {
-            type: Number,
-            required: true,
-        },
-        remaining: {
-            type: Number,
-            required: true,
-        },
-    }
+      amount: {
+        type: Number,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      totalPrice: {
+        type: Number,
+        required: true,
+      },
+      paid: {
+        type: Number,
+        required: true,
+      },
+      remaining: {
+        type: Number,
+        required: true,
+      },
+    },
   },
   {
     timestamps: true,
   }
 );
+
+tourSchema.methods.minusTickets = async function (amount) {
+  this.tickets.amount -= amount;
+  await this.save();
+  return this.tickets;
+};
+
+tourSchema.methods.addTickets = async function (amount) {
+  this.tickets.amount += amount;
+  await this.save();
+  return this.tickets;
+};
 
 const Tour = mongoose.model('Tour', tourSchema);
 module.exports = Tour;
