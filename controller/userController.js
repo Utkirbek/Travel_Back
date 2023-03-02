1713176f3e2b736e6f94a96cd247d0f0de2a15df
Utkirbek <dovutkhanmutaliyev@gmail.com>
2023-03-02 19:41:48 +0500

    some changes

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,7 @@
 const User = require('../models/User');
 const Tour = require('../models/Tour');
+const Kassa = require('../models/Kassa');
+const Profit = require('../models/Profit');
 
 const addUser = async (req, res) => {
   try {
@@ -11,6 +13,25 @@ const addUser = async (req, res) => {
     } else {
       res.status(404).send({ message: 'Tour not found!' });
     }
+    const kassa = await Kassa.find({ branch: req.params.branch })
+      .sort({ _id: -1 })
+      .limit(1);
+
+    if (kassa) {
+      await kassa[0].addAmount(req.body.paid);
+    } else {
+      res.status(404).send({ message: 'Kassa not found!' });
+    }
+    const profit = await Profit.find({ branch: req.params.branch })
+      .sort({ _id: -1 })
+      .limit(1);
+    let profitAmount = tour.tickets.price - req.body.price;
+    if (profit) {
+      await profit[0].addAmount(profitAmount);
+    } else {
+      res.status(404).send({ message: 'Profit not found!' });
+    }
+
     res.send({ message: 'User Added Successfully!' });
   } catch (err) {
     res.status(500).send({ message: err.message });
