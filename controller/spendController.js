dddb9088dd61e7a91927b230dcc01201eba17741
Utkirbek <utkirbel.me@gmail.com>
2023-04-12 15:28:25 +0500

    sooem changes

--- controller/spendController.js
+++ controller/spendController.js
@@ -2,8 +2,21 @@ const Spend = require('../models/Spend');
 
 const addSpend = async (req, res) => {
   try {
-    const newSpend = new Spend(req.body);
-    await newSpend.save();
+    const newSpend = req.body;
+    newSpend.makkah.room.total = newSpend.makkah.room.price * newSpend.makkah.room.amount
+    newSpend.makkah.food.total = newSpend.makkah.food.price * newSpend.makkah.food.amount
+    newSpend.makkah.total = (newSpend.makkah.room.total + newSpend.makkah.food.total)*newSpend.makkah.days
+
+    newSpend.madina.room.total = newSpend.madina.room.price * newSpend.madina.room.amount
+    newSpend.madina.food.total = newSpend.madina.food.price * newSpend.madina.food.amount
+    newSpend.madina.total = (newSpend.madina.room.total + newSpend.madina.food.total)*newSpend.madina.days
+
+    newSpend.total = newSpend.makkah.total + newSpend.madina.total + newSpend.extra
+
+    const spend = new Spend(newSpend);
+    await spend.save();
+
+
     res.send({ message: 'Spend Added Successfully!' });
   } catch (err) {
     res.status(500).send({ message: err.message });
